9b3a0e5917ecef7f0da1969d7d75a738f29e88d3
Volodymyr Volunets <slakeer@DESKTOP-H23HPN7.>
2024-12-03 23:16:06 +0000

    feat: added some pages/components

---
+++ frontend/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Header from './components/header.js'
+import HomePage from './pages/HomePage';
+
+function App() {
+  return (
+    <div className="App">
+      <Header/>
+      <HomePage />
+    </div>
+  );
+}
+
+export default App;
